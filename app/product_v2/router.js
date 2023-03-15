9221e20527393dc16b8e70981487f5fa76b9a9b8
LintangVienusa <vienusa6661@gmail.com>
2023-03-15 17:23:34 +0700

    add method put and delete using sequelize (nodejs x expresjs)

--- app/product_v2/router.js
+++ app/product_v2/router.js
@@ -24,23 +24,55 @@ router.post('/catalog', upload.single('image'), async(req, res) => {
 
 router.get('/catalog', async(req, res) => {
     try {
-        await Catalog.sync();
+        Catalog.sync();
         const result = Catalog.findAll({raw: true})
-        _response(result)
-        console.log(JSON.stringify(result))
+        res.send(await result)
     }catch(e) {
         res.send(e)
     }
 }) 
-// , ()=> {
-    // console.log("test");
-    // try {
-    //     await Catalog.sync();
-    //     const result = Catalog.findAll()
-    //     res.send(result)
-    // }catch(e) {
-    //     res.send(e)
-    // }
-// }
+
+router.get('/catalog/:id', async(req, res) => {
+    const id = req.params.id
+    try {
+        Catalog.sync();
+        const result = Catalog.findAll({where:{id: id}})
+        res.send(await result)
+    }catch(e) {
+        res.send(e)
+    }
+}) 
+
+router.put('/catalog/:id', upload.single('image'), async(req, res) => {
+    const  { id, name, stock, is_display } = req.body
+    const image = req.file;
+    if(image) {
+        const target = path.join(__dirname, '../../uploads', image.originalname);
+        fs.renameSync(image.path, target);
+        try {
+            Catalog.sync();
+            const result = Catalog.update(
+                { id, name, stock, is_display, image_path: `http://localhost:3000/public/${image.originalname}` }, {
+                    where: {id: id}
+                }
+            )
+            res.send(await result);
+        }catch(e) {
+            res.send(await e)
+        }
+    }
+})
+
+router.delete('/catalog/:id', async(req, res) => {
+    const id = req.params.id
+    try {
+        Catalog.sync();
+        const result = Catalog.destroy({where:{id: id}})
+        res.send(await result);
+    }catch(e) {
+        res.send(e)
+    }
+}) 
+
 
 module.exports = router;
